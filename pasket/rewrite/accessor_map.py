11dae7923870be350775f5aa475d3e4a325235a3
Jinseong Jeon <jsjeon@umiacs.umd.edu>
2015-02-22 22:23:22 -0500

    finding candidate classes

--- pasket/rewrite/accessor_map.py
+++ pasket/rewrite/accessor_map.py
@@ -1,6 +1,9 @@
+import logging
+
 import lib.const as C
 import lib.visit as v
 
+from .. import util
 from ..meta.template import Template
 from ..meta.clazz import Clazz
 from ..meta.method import Method
@@ -15,6 +18,7 @@ class AccessorMap(object):
     self._acc_default = acc_default
     self._acc_conf = acc_conf
 
+    self._clss = []
     self._aux_name = C.ACC.AUX+"Map"
     self._aux = None
 
@@ -36,8 +40,22 @@ class AccessorMap(object):
     This is the generic method to initialize the dynamic dispatcher
     """
 
+  # find candidate classes that have candidate getter/setter of Map type
+  def find_clss_involved(self, tmpl):
+    clss = util.flatten_classes(tmpl.classes, "inners")
+    self._clss = filter(AccessorMap.is_candidate_cls, clss)
+    logging.debug("{} candidates: {}".format(self._aux_name, self._clss))
+
+  @staticmethod
+  def is_candidate_cls(cls):
+    mtds = filter(lambda m: not m.is_init and not m.is_static, cls.mtds)
+    getters = filter(lambda m: len(m.params) == 1 and m.typ != C.J.v, mtds)
+    setters = filter(lambda m: len(m.params) == 2, mtds)
+    return cls.is_class and (any(getters) or any(setters))
+
   @v.when(Template)
-  def visit(self, node): pass
+  def visit(self, node):
+    self.find_clss_involved(node)
 
   @v.when(Clazz)
   def visit(self, node): pass
