589b53d5638955076a9974634835aaf63ce59943
Jinseong Jeon <jsjeon@umiacs.umd.edu>
2015-03-06 10:58:41 -0500

    more APIs that correspond to method roles for DocumentEvent

--- template/gui/swing/text/AbstractDocument.java
+++ template/gui/swing/text/AbstractDocument.java
@@ -2,6 +2,12 @@ package javax.swing.text;
 
 public abstract class AbstractDocument implements Document {
   public void addDocumentListener(DocumentListener listener);
+  public void removeDocumentListener(DocumentListener listener);
+
+  protected void fireChangeUpdate(DocumentEvent e);
+  protected void fireInsertUpdate(DocumentEvent e);
+  protected void fireRemoveUpdate(DocumentEvent e);
+
   public int getLength();
   public String getText(int offset, int length);
 
