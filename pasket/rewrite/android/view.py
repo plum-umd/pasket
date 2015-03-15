a0fdff89fe50d70d9ad4d69050d8c57246e92550
Jinseong Jeon <jsjeon@umiacs.umd.edu>
2015-03-15 00:35:53 -0400

    View hierarchy buildup and lookup

--- pasket/rewrite/android/view.py
+++ pasket/rewrite/android/view.py
@@ -7,7 +7,7 @@ from ...meta.template import Template
 from ...meta.clazz import Clazz
 from ...meta.method import Method
 from ...meta.field import Field
-from ...meta.statement import Statement
+from ...meta.statement import Statement, to_statements
 from ...meta.expression import Expression
 
 class View(object):
@@ -23,8 +23,20 @@ class View(object):
   @v.when(Template)
   def visit(self, node): pass
 
+  @staticmethod
+  def add_fld(cls, ty, nm):
+    logging.debug("adding field {}.{} of type {}".format(cls.name, nm, ty))
+    fld = Field(clazz=cls, typ=ty, name=nm)
+    cls.add_flds([fld])
+    cls.init_fld(fld)
+    return fld
+
   @v.when(Clazz)
-  def visit(self, node): pass
+  def visit(self, node):
+    cname = node.name
+    if cname in [C.ADR.VG, C.ADR.WIN]:
+      fld = View.add_fld(node, u"List<{}>".format(C.ADR.VIEW), u"mChildren")
+      setattr(node, "children", fld)
 
   @v.when(Field)
   def visit(self, node): pass
@@ -32,12 +44,82 @@ class View(object):
   @v.when(Method)
   def visit(self, node):
     cname = node.clazz.name
+    mname = node.name
 
-    if cname == C.ADR.VG: # ViewGroup
-      pass
+    ##
+    ## View hierarchy buildup
+    ##
 
-    elif cname == C.ADR.WIN: # Window
-      pass
+    if cname in [C.ADR.VG, C.ADR.WIN]:
+      fld = getattr(node.clazz, "children")
+      fname = fld.name
+      if mname == "addView": # ViewGroup.addView
+        _, v = node.params[0]
+        body = u"""
+          {fname}.add({v});
+        """.format(**locals())
+        node.body = to_statements(node, body)
+
+      elif mname.endswith("ContentView"): # (set|add)
+        _, v = node.params[0]
+        body = u"""
+          {fname}.add({v});
+        """.format(**locals())
+        node.body = to_statements(node, body)
+
+    ##
+    ## View lookup
+    ##
+
+    if mname.startswith("find"+C.ADR.VIEW):
+      _, _id = node.params[0]
+
+      if cname == C.ADR.VIEW:
+        ##
+        ## public *final* View View.findViewById(int id)
+        ##
+        if mname.endswith("ById"):
+          body = u"""
+            if ({_id} < 0) return null;
+            return this.findViewTraversal({_id});
+          """.format(**locals())
+          node.body = to_statements(node, body)
+
+        ##
+        ## *protected* View View.findViewTraversal(int id)
+        ##
+        else: # overridable traversal
+          body = u"""
+            int me = this.getId();
+            if ({_id} == me) return this;
+            else return null;
+          """.format(**locals())
+          node.body = to_statements(node, body)
+
+      elif cname in [C.ADR.VG, C.ADR.WIN]:
+        ##
+        ## protected View ViewGroup.findViewTraversal(int id)
+        ## public View Window.findViewById(int id)
+        ##
+        fld = getattr(node.clazz, "children")
+        fname = fld.name
+        traversal = u"""
+          for (View v : {fname}) {{
+            int v_id = v.getId();
+            if (v_id == {_id}) return v;
+          }}
+          return null;
+        """.format(**locals())
+
+        if cname == C.ADR.VG:
+          check_myself = u"""
+            int me = this.getId();
+            if ({_id} == me) return this;
+          """.format(**locals())
+          traversal = check_myself + traversal
+
+        node.body = to_statements(node, traversal)
+
 
   @v.when(Statement)
   def visit(self, node): return [node]
