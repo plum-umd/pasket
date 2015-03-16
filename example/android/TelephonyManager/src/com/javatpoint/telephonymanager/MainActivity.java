f6bc6a28d3f05354086e5f3cf980b55696af6656
Jon Fetter-Degges <jonfetterdegges@gmail.com>
2015-03-16 18:05:23 -0400

    hardcoded setContentView

--- example/android/TelephonyManager/src/com/javatpoint/telephonymanager/MainActivity.java
+++ example/android/TelephonyManager/src/com/javatpoint/telephonymanager/MainActivity.java
@@ -7,15 +7,18 @@ import android.telephony.TelephonyManager;
 import android.view.Menu;
 import android.widget.TextView;
 
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+
 public class MainActivity extends Activity {
 	TextView textView1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
         textView1=(TextView)findViewById(R.id.textView1);
-        
+
         TelephonyManager  tm=(TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
         String IMEINumber=tm.getDeviceId();
         String subscriberID=tm.getDeviceId();
@@ -24,28 +27,28 @@ public class MainActivity extends Activity {
         String SIMCountryISO=tm.getSimCountryIso();
         String softwareVersion=tm.getDeviceSoftwareVersion();
         String voiceMailNumber=tm.getVoiceMailNumber();
-        
+
         //Get the phone type
         String strphoneType="";
-        
+
         int phoneType=tm.getPhoneType();
 
-        switch (phoneType) 
+        switch (phoneType)
         {
                 case (TelephonyManager.PHONE_TYPE_CDMA):
                            strphoneType="CDMA";
                                break;
-                case (TelephonyManager.PHONE_TYPE_GSM): 
-                           strphoneType="GSM";              
+                case (TelephonyManager.PHONE_TYPE_GSM):
+                           strphoneType="GSM";
                                break;
                 case (TelephonyManager.PHONE_TYPE_NONE):
-                			strphoneType="NONE";              
+                			strphoneType="NONE";
                                 break;
          }
-        
+
         //getting information if phone is in roaming
         boolean isRoaming=tm.isNetworkRoaming();
-        
+
         String info="Phone Details:\n";
         info+="\n IMEI Number:"+IMEINumber;
         info+="\n SubscriberID:"+subscriberID;
@@ -56,9 +59,26 @@ public class MainActivity extends Activity {
         info+="\n Voice Mail Number:"+voiceMailNumber;
         info+="\n Phone Network Type:"+strphoneType;
         info+="\n In Roaming? :"+isRoaming;
-        
+
         textView1.setText(info);
     }
 
-   
+  @Override
+  @SuppressWarnings("unused")
+  public void setContentView(int id) {
+    RelativeLayout top_layout = new RelativeLayout(this);
+    top_layout.setLayoutParams(
+      new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                                 ViewGroup.LayoutParams.WRAP_CONTENT));
+    TextView tv = new TextView(this);
+    tv.setId(R.id.textView1);
+    tv.setText("Phone Details:");
+
+    top_layout.addView(tv, 0,
+      new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT,
+                                      RelativeLayout.LayoutParams.WRAP_CONTENT));
+
+    setContentView(top_layout);
+  }
+
 }
