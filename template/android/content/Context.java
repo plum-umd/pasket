0d9e42df7018df7696ab113588da6a7063ed2c4d
Jinseong Jeon <jsjeon@umiacs.umd.edu>
2015-03-21 12:18:25 -0400

    APIs for TelephonyManager tutorial

--- template/android/content/Context.java
+++ template/android/content/Context.java
@@ -1,6 +1,8 @@
 package android.content;
 
 public abstract class Context {
+  public final static String TELEPHONY_SERVICE = "phone";
+
   public abstract void startActivity(Intent intent);
   public abstract void startActivity(Intent intent, Bundle options);
 
