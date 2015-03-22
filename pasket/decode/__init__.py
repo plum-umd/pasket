3e061a0b4f3f192c0bd377993e1c85aead709235
Jinseong Jeon <jsjeon@umiacs.umd.edu>
2015-03-22 03:36:07 -0400

    collect and import packages

--- pasket/decode/__init__.py
+++ pasket/decode/__init__.py
@@ -19,7 +19,7 @@ from adapter import Adapter
 from observer import Observer
 from singleton import Singleton
 
-pkgs_android = [u"android."]
+pkgs_android = [u"android.", u"com.android."]
 
 pkgs_gui = [u"java.awt", u"javax.swing", u"javax.accessibility"]
 
@@ -266,10 +266,15 @@ def dump(cmd, dst_dir, tmpl, msg=None):
       u"InputStreamReader", u"BufferedReader", \
       u"FileNotFoundException", u"IOException"]
 
-  gui_pkgs = set([])
+  if cmd == "android":
+    pkgs_of_interest = pkgs_android
+  elif cmd == "gui":
+    pkgs_of_interest = pkgs_gui
+  decl_pkgs = set([])
   for cls in tmpl.classes:
     if not cls.pkg: continue
-    if check_pkg(cls.pkg, pkgs_gui): gui_pkgs.add(cls.pkg)
+    if check_pkg(cls.pkg, pkgs_of_interest):
+      decl_pkgs.add(cls.pkg)
 
   for cls in tmpl.classes:
     ## generate folders according to package hierarchy
@@ -286,13 +291,11 @@ def dump(cmd, dst_dir, tmpl, msg=None):
     cls_body = str(cls)
     imports.extend(find_imports(cls_body, u"java.util", C.collections))
     imports.extend(find_imports(cls_body, u"java.io", ios))
-
-    if cmd == "android":
-      pass
-    elif cmd == "gui":
+    if cmd == "gui":
       imports.extend(find_imports(cls_body, u"java.util", [C.GUI.EVT]))
-      for pkg in gui_pkgs:
-        if not cls.pkg or cls.pkg != pkg: imports.append(pkg+".*")
+
+    for pkg in decl_pkgs:
+      if not cls.pkg or cls.pkg != pkg: imports.append(pkg+".*")
 
     ## generate Java files
     with open(java_path, 'w') as f:
