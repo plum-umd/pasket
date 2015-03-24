ad8784fa207cfbae85e28a63746281639dc82ec8
Jinseong Jeon <jsjeon@umiacs.umd.edu>
2015-03-24 16:32:20 -0400

    desugaring: avoid String concatenation

--- example/android/UIButton/src/course/examples/UI/Button/ButtonActivity.java
+++ example/android/UIButton/src/course/examples/UI/Button/ButtonActivity.java
@@ -9,41 +9,42 @@ import android.widget.Button;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 
-public class ButtonActivity extends Activity {
-	int count = 0;
+public class ButtonActivity extends Activity implements OnClickListener {
+    int count = 0;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        final Button button = (Button) findViewById(R.id.button);
+        Button button = (Button) findViewById(R.id.button);
 
         if (button.isEnabled()) {
-          button.setOnClickListener(new OnClickListener() {
-              @Override
-              public void onClick(View v) {
-                button.setText("Got Pressed:" + ++count);
-              }
-            });
+            button.setOnClickListener(this);
         }
     }
 
-  @Override
-  public void setContentView(int id) {
-    RelativeLayout top_layout = new RelativeLayout(this);
-    top_layout.setLayoutParams(
-        new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
-                                   ViewGroup.LayoutParams.MATCH_PARENT));
-    Button btn = new Button(this);
-    btn.setId(R.id.button);
-    btn.setText("Press Me!");
-    RelativeLayout.LayoutParams btnLP =
-      new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT,
-                                      RelativeLayout.LayoutParams.WRAP_CONTENT);
-    btnLP.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
-    btnLP.setMargins(10, 0, 0, 0);
-    top_layout.addView(btn, 0, btnLP);
+    @Override
+    public void onClick(View v) {
+        //button.setText("Got Pressed:" + ++count);
+        count = count + 1;
+    }
 
-    setContentView(top_layout);
-  }
+    @Override
+    public void setContentView(int id) {
+        RelativeLayout top_layout = new RelativeLayout(this);
+        top_layout.setLayoutParams(
+            new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                                       ViewGroup.LayoutParams.MATCH_PARENT));
+        Button btn = new Button(this);
+        btn.setId(R.id.button);
+        btn.setText("Press Me!");
+        RelativeLayout.LayoutParams btnLP =
+          new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT,
+                                          RelativeLayout.LayoutParams.WRAP_CONTENT);
+        btnLP.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+        btnLP.setMargins(10, 0, 0, 0);
+        top_layout.addView(btn, 0, btnLP);
+
+        setContentView(top_layout);
+    }
 }
