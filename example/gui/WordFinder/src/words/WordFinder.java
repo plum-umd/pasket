b184b34777b8e5c70fd95c798a9e5609f86619bd
Jinseong Jeon <jsjeon@umiacs.umd.edu>
2015-03-23 22:15:38 -0400

    desugaring: catch exceptions, rather throw them

--- example/gui/WordFinder/src/words/WordFinder.java
+++ example/gui/WordFinder/src/words/WordFinder.java
@@ -25,32 +25,32 @@ import java.awt.BorderLayout;
 public class WordFinder extends JFrame {
 
     private static final long serialVersionUID = 1L;
-    
+
     private final WordList words = new WordList();
-    
+
     private final JTextField find;
-    
+
     private final JList list;
-    
+
     private final JLabel counter;
-    
+
     /**
      * Make a WordFinder window.
      */
     public WordFinder() {
         super("Word Finder");
-        
+
         // call System.exit() when user closes the window
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        
+
         Container cp = this.getContentPane();
         /*
          * Task 2: set the layout manager of the content pane to a TableLayout.
          */
         cp.setLayout(new BorderLayout());
-        
+
         cp.add(new JLabel("Find: "));
-        
+
         find = new JTextField(20);
         cp.add(find);
         /*
@@ -62,20 +62,20 @@ public class WordFinder extends JFrame {
                 doFind();
             }
         });
-        
+
         /*
          * Task 3: add a JList inside a JScrollPane that shows matching words
          */
         list = new JList(new DefaultListModel());
         JScrollPane scroller = new JScrollPane(list);
         cp.add(scroller);
-        
+
         /*
          * Task 4: add a JLabel that shows the number of matching words
          */
         counter = new JLabel("");
         cp.add(counter);
-        
+
         /*
          * Task 5: add a Search button
          */
@@ -86,7 +86,7 @@ public class WordFinder extends JFrame {
             } 
         });
         cp.add(search);
-        
+
         JButton clear = new JButton("Clear");
         clear.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
@@ -94,13 +94,13 @@ public class WordFinder extends JFrame {
             } 
         });
         cp.add(clear);
-        
+
         /*
          * Task 6: add a File menu with Open... and Exit options
          */
         JMenuBar menubar = new JMenuBar();
         JMenu fileMenu = new JMenu("File");
-        
+
         JMenuItem openItem = new JMenuItem("Open...");
         fileMenu.add(openItem);
         openItem.addActionListener(new ActionListener() {
@@ -108,7 +108,7 @@ public class WordFinder extends JFrame {
                 doOpen();
             }
         });
-        
+
         JMenuItem exitItem = new JMenuItem("Exit");
         exitItem.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
@@ -116,40 +116,40 @@ public class WordFinder extends JFrame {
             }
         });
         fileMenu.add(exitItem);
-        
+
         menubar.add(fileMenu);
         setJMenuBar(menubar);
-        
+
         this.pack();
-        
+
         try {
             InputStream in = new FileInputStream("words.txt");
             loadWords(in);
         } catch (IOException ioe) { }
         doFind();
     }
-    
+
     private void doClear() {
         find.setText("");
         doFind();
         find.grabFocus();
     }
-    
+
     private void doFind() {
         String query = find.getText();
-        
+
         List<String> matches = words.find(query);
         DefaultListModel listModel = (DefaultListModel) list.getModel();
         listModel.removeAllElements();
         for (String match : matches) {
             listModel.addElement(match);
         }
-        
+
         updateCounter(query);
         find.selectAll();
         find.grabFocus();
     }
-    
+
     private void updateCounter(String query) {
         int n = list.getModel().getSize();
         String msg;
@@ -157,30 +157,28 @@ public class WordFinder extends JFrame {
         else msg = n + " matches for '" + query + "'";
         counter.setText(msg);
     }
-    
-    private void loadWords(InputStream in) throws IOException {
+
+    private void loadWords(InputStream in) {
         words.load(in);
         doClear();
     }
-    
+
     private void doOpen() {
         JFileChooser chooser = new JFileChooser();
         int returnVal = chooser.showOpenDialog(this);
         if(returnVal == JFileChooser.APPROVE_OPTION) {
-            try {
-                loadFile(chooser.getSelectedFile());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            loadFile(chooser.getSelectedFile());
         }        
     }
-    
-    private void loadFile(File file) throws IOException {
-        FileInputStream in = new FileInputStream(file);
-        loadWords(in);
-        in.close();
+
+    private void loadFile(File file) {
+        try {
+            FileInputStream in = new FileInputStream(file);
+            loadWords(in);
+            in.close();
+        } catch (IOException e) { }
     }
-    
+
     /**
      * Main method.  Makes and displays a WordFinder window.
      * @param args Command-line arguments.  Ignored.
--- example/gui/WordFinder/src/words/WordList.java
+++ example/gui/WordFinder/src/words/WordList.java
@@ -4,6 +4,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.FileNotFoundException;
 import java.io.StreamTokenizer;
 import java.text.Collator;
 import java.util.ArrayList;
@@ -13,53 +14,54 @@ import java.util.Set;
 import java.util.TreeSet;
 
 /**
- * A WordList is a set of words that is searchable by substring.  
+ * A WordList is a set of words that is searchable by substring.
  * A word is defined as a sequence of letters
  * (upper case or lower case) or apostrophes (to allow contractions
  * like "don't").  WordLists ignore alphabetic case when
- * searching and sorting. 
+ * searching and sorting.
  */
 public class WordList {
     private final List<String> words = new ArrayList<String>();
-    
+
     /*
      * Rep invariant:
      *      words != null
      */
-    
+
     /**
      * Make an empty WordList.
      */
     public WordList() { }
-    
+
     /**
      * Load a stream into this word set.
      * @requires in is an open stream consisting of a sequence of words.
-     * @effects Removes all the words from this word set and replaces 
+     * @effects Removes all the words from this word set and replaces
      * them with the words found in the stream (treating punctuation,
      * whitespace, and numbers as delimiters between words).
-     * @throws IOException if an error occurred while reading the stream
      */
-    public void load(InputStream in) throws IOException {
+    public void load(InputStream in) {
         Collator c = Collator.getInstance();
         c.setStrength(Collator.PRIMARY);
         Set<String> s = new TreeSet<String>(c);
-        
+
         StreamTokenizer tok = new StreamTokenizer(new InputStreamReader(in));
         tok.resetSyntax();
         tok.wordChars('a', 'z');
         tok.wordChars('A', 'Z');
         tok.wordChars('\'', '\'');
-        
-        while (tok.nextToken() != StreamTokenizer.TT_EOF) {
-            if (tok.ttype == StreamTokenizer.TT_WORD)
-                s.add(tok.sval);
-        }
-        
+
+        try {
+            while (tok.nextToken() != StreamTokenizer.TT_EOF) {
+                if (tok.ttype == StreamTokenizer.TT_WORD)
+                    s.add(tok.sval);
+            }
+        } catch (IOException e) { }
+
         words.clear();
         words.addAll(s);
     }
-    
+
     /**
      * Find words containing a given substring.
      * @param s Substring to search for
@@ -72,7 +74,7 @@ public class WordList {
         if (s.length() == 0) {
             return Collections.unmodifiableList(words);
         }
-        
+
         s = s.toLowerCase();
         List<String> l = new ArrayList<String>();
         for (String word : words) {
@@ -81,22 +83,23 @@ public class WordList {
         }
         return l;
     }
-    
+
     /**
      * Main method.  Demonstrates how to use this class.
      * @param args Command-line arguments.  Ignored.
      */
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         WordList words = new WordList ();
-        
-        InputStream in = new FileInputStream("words");
-        
-        words.load(in);
-        
+
+        try {
+            InputStream in = new FileInputStream("words");
+            words.load(in);
+        } catch (FileNotFoundException e) { }
+
         // Print all the words containing "ph"
         List<String> matches = words.find("ph");
         for (String match : matches) {
             System.out.println(match);
         }
-    }    
+    }
 }
