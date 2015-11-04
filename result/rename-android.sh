ca9531dcd4d41821a58dfc511babf7dadb4339c6
Jinseong Jeon <jsjeon@umiacs.umd.edu>
2015-11-04 00:16:11 -0500

    rm *-e files if exist ('sed' may behave differently in different OSes)

--- result/rename-android.sh
+++ result/rename-android.sh
@@ -6,4 +6,6 @@ for i in `find "$JAVA" -type f -name '*java'`;
 do
   sed -i -e 's/ android./ symdroid.model.android./' $i
   sed -i -e 's/ com.android./ symdroid.model.com.android./' $i
+
+  rm -f "$i-e"
 done
--- result/rename-gui.sh
+++ result/rename-gui.sh
@@ -13,7 +13,6 @@ rm -rf $JAVA/javax
 
 for i in `find "$JAVA/pasket" -type f -name '*java'`;
 do
-
   sed -i -e 's/package java.util;/package pasket.java.util;/' $i
   sed -i -e 's/java.util.EventObject/pasket.java.util.EventObject/' $i
 
@@ -21,4 +20,5 @@ do
   sed -i -e 's/javax.swing/pasket.javax.swing/' $i
   sed -i -e 's/javax.accessibility/pasket.javax.accessibility/' $i
 
+  rm -f "$i-e"
 done
