855d4a60452f0ce9bc9a1f3288a9d044787023fe
Jinseong Jeon <jsjeon@umiacs.umd.edu>
2016-02-07 18:44:34 -0500

    removed failed output

--- result/exp.py
+++ result/exp.py
@@ -7,7 +7,7 @@ import sys
 
 demos = {
   "android": ["button", "checkbox", "telephony_manager"],
-  "gui": ["button_demo", "checkbox_demo", "colorchooser_demo", "combobox_demo", "custom_icon_demo", "filechooser_demo", "toolbar_demo", "menu_demo", "splitpane_divider_demo", "text_field_demo"]
+  "gui": ["checkbox_demo", "colorchooser_demo", "combobox_demo", "custom_icon_demo", "filechooser_demo", "toolbar_demo", "button_demo", "menu_demo", "splitpane_divider_demo", "text_field_demo"]
 }
 
 output = "output"
@@ -18,19 +18,19 @@ def main():
   opts.extend(["--slv-timeout", "30"])
   opts.extend(["--slv-parallel", "--slv-randassign", "--slv-strategy", "WILCOXON"])
   opts.extend(["--slv-p-cpus", "32"])
-  opts.extend(["--slv-ntimes", str(1024*64)])
+  opts.extend(["--slv-ntimes", str(1024*128)])
 
   r_cmds = []
   r_cmds.extend(["sketch"])
   r_cmds.extend(opts)
   for cmd in demos:
     tutorials = demos[cmd]
-    for tutorial in tutorials:
-      _r_cmds = r_cmds[:]
-      dp = "sk_{}".format(tutorial)
-      _r_cmds.extend(["--fe-inc", dp])
-      _r_cmds.extend([os.path.join(dp, "sample.sk")])
-      for i in xrange(7):
+    for i in xrange(7):
+      for tutorial in tutorials:
+        _r_cmds = r_cmds[:]
+        dp = "sk_{}".format(tutorial)
+        _r_cmds.extend(["--fe-inc", dp])
+        _r_cmds.extend([os.path.join(dp, "sample.sk")])
         print " ".join(_r_cmds)
         now = int(datetime.datetime.now().strftime("%H%M%S"))
         op = os.path.join(output, "{}_{}.txt".format(tutorial, now))
